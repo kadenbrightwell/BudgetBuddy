5e8f9bfc47ae1ec03a859d1ce7142505cf5b4e31
Kaden Brightwell <61510196+kadenbrightwell@users.noreply.github.com>
2025-12-10 19:57:31 -0500

    Create User and Expense models with relationships

---
+++ models.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from datetime import datetime
+
+db = SQLAlchemy()
+
+# User Table
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(150), nullable=False)
+    expenses = db.relationship('Expense', backref='author', lazy=True)
+
+# Expense Table (The Data)
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    category = db.Column(db.String(50), nullable=False) # e.g., Food, Rent
+    amount = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
